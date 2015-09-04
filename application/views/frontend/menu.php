6f8aa8520c880dcb325fa3e8aa98495ca4726125
Ampare <kamonchatsinchai305@gmail.com>
2015-09-04 13:03:05 +0700

    edit

--- application/views/frontend/menu.php
+++ application/views/frontend/menu.php
@@ -5,8 +5,8 @@
     <img src="<?php echo base_url("asset/images/banner1.png")?> " class='img-banner'> </div>
     <div class="navbar-collapse collapse">
       <ul class="nav navbar-nav navbar-right nav-menu">
-			<li class="active"><a href="<?php echo base_url("frontend/index");?>"> <i class="fa fa-home"></i>  หน้าหลัก</a></li>
-			<li><a href="<?php echo base_url("frontend/about");?>"><i class="fa fa-university"></i> เกี่ยวกับเรา</a></li>
+			<li class="active"><a href=""> <i class="fa fa-home"></i>  หน้าหลัก</a></li>
+			<li><a href=""><i class="fa fa-university"></i> เกี่ยวกับเรา</a></li>
 			<li><a href="webboard.php"><i class="fa fa-comments"></i> เว็บบอร์ด</a></li>
 			<li><a href="contact.html"><i class="fa fa-phone"></i> ติดต่อ</a></li>
 			<li class="dropdown "><a  data-toggle="dropdown" class="" href="#"><i class="fa fa-key"></i> เข้าสู่ระบบ  <span class="caret"></span></a>
