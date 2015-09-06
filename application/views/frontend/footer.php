b5e6f46e583794113976efe9e498835e2f1eb99b
Ampare <kamonchatsinchai305@gmail.com>
2015-09-06 12:48:21 +0700

    Edit

--- application/views/frontend/footer.php
+++ application/views/frontend/footer.php
@@ -4,10 +4,10 @@
     <div class="row">
       
                 <div class="col-md-offset-1 col-md-5 padd">
-                    <h4>ติดต่อ</h4>
+                    <strong><h4 class="piksell-color">ติดต่อ</h4></strong>
                     <div class="line-left"></div>
                     <p class="styletext">
-					<strong><font class="piksell-color">สถาบันบ่มเพาะวิสาหกิจ</font></strong>อาคารอำนวยยศสุข มหาวิทยาลัยแม่โจ้ 
+					<font>สถาบันบ่มเพาะวิสาหกิจ</font>อาคารอำนวยยศสุข มหาวิทยาลัยแม่โจ้ 
 					<br>
 					<i class="fa fa-map-marker"></i>&nbsp;63 ม.4 ต.หนองหาร อ.สันทราย จ.เชียงใหม่ 50290<br>
                     <i class="fa fa-phone"></i>&nbsp0-5387-5326 , 08-1883-2696<br>
@@ -17,13 +17,15 @@
 				</div>
 				
 				<div class="col-md-offset-1 col-md-5 padd">
-                    <h4>บริการ</h4>
+                    <strong><h4 class="piksell-color">บริการ</h4></strong>
                     <div class="line-left"></div>
                     <p class="styletext">
-					<strong><font class="piksell-color">สถาบันบ่มเพาะวิสาหกิจ</font></strong>อาคารอำนวยยศสุข มหาวิทยาลัยแม่โจ้ 
 					<br>
-					<i class="fa fa-fax"></i>&nbsp;0-5387-5327<br>
-                    <i class="fa fa-envelope"></i>&nbsp;mjubi.mju@mju.ac.th
+				<a href="" style="font-size: 15px;margin-left: 0px;">service platform</a><br>
+               <a href="http://stdb.most.go.th/Home"style="font-size: 15px;margin-left: 0px;">ฐานข้อมูลโครงสร้างพื้นฐาน ภาครัฐ</a><br>  
+                <a href=""style="font-size: 15px;margin-left: 0px;">IRCT platform</a><br>
+                <a href=""style="font-size: 15px;margin-left: 0px;">Core server platform</a><br>
+                <a href=""style="font-size: 15px;margin-left: 0px;">บ่มเพาะ TBI platform </a> 
 				</div>
     </div>
   </div>
