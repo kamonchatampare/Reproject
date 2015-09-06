1fba01aabcce61986415bf6a5fdef3e129ce1d29
Ampare <kamonchatsinchai305@gmail.com>
2015-09-06 12:33:03 +0700

    editEdit

--- application/views/frontend/footer.php
+++ application/views/frontend/footer.php
@@ -3,26 +3,20 @@
   <div class="container">
     <div class="row">
       
-                <div class="col-md-offset-1 col-md-5 padd">
-                    <h5>Mjubi</h5>
+                <div class="col-md-offset-4 col-md-5 padd">
+                    <h5>ติดต่อ</h5>
                     <div class="line-left"></div>
                     <p class="styletext">
 					<strong><font class="piksell-color">สถาบันบ่มเพาะวิสาหกิจ</font></strong>อาคารอำนวยยศสุข มหาวิทยาลัยแม่โจ้ 
 					<br>
 					<i class="fa fa-map-marker"></i>&nbsp;63 ม.4 ต.หนองหาร อ.สันทราย จ.เชียงใหม่ 50290<br>
                     <i class="fa fa-phone"></i>&nbsp0-5387-5326 , 08-1883-2696<br>
+					<i class="fa fa-fax"></i>&nbsp;0-5387-5327<br>
+                    <i class="fa fa-envelope"></i>&nbsp;mjubi.mju@mju.ac.th
 					
 				</div>
 				
-				<div class="col-md-offset-1 col-md-5 padd">
-                    <h5>Mjubi</h5>
-                    <div class="line-left"></div>
-                    <p class="styletext">
-					<strong><font class="piksell-color">สถาบันบ่มเพาะวิสาหกิจ</font></strong>อาคารอำนวยยศสุข มหาวิทยาลัยแม่โจ้ 
-					<br>
-					<i class="fa fa-fax"></i>&nbsp;0-5387-5327<br>
-                    <i class="fa fa-envelope"></i>&nbsp;mjubi.mju@mju.ac.th
-				</div>
+		
     </div>
   </div>
 </div>
